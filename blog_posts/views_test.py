5c3e23a625a123f44740a96799533b036ee7c866
Rachell Calhoun <rachell.calhoun@gmail.com>
2022-10-16 14:43:37 -0400

    authentication

--- blog_posts/views_test.py
+++ blog_posts/views_test.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import Page
-
+from . models import BlogPost
 
 def test_list(page: Page, blog_post_factory):
     post1, post2, post3 = blog_post_factory.create_batch(3)
@@ -14,3 +14,19 @@ def test_detail(page: Page, blog_post):
     page.click(f"text={blog_post.name}")
 
     assert page.locator("h1").inner_text() == blog_post.name
+
+
+def test_create(page):
+    page.goto("/")
+    page.click("text=Publish")
+    page.fill("text=Name", "Hello World!")
+    page.fill("text=Text", "I wrote some text")
+    # context manager to save and reload before it continues
+    with page.expect_navigation():
+        page.click("text=Save")
+
+    blog_post = BlogPost.objects.get()
+    assert blog_post.author == page.user
+    assert blog_post.name == "Hello World!"
+    assert blog_post.text == "I wrote some text"
+    assert page.locator("h1").inner_text() == "Hello World!"
